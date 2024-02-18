7af2ed09a9d2a6f4ee69efd63f23216cc2c7547e
Steffano Pereira <steffano.pereira@ufrpe.br>
2024-02-18 12:43:24 -0300

    refactor(ep01): create login as support command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,19 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('login', (email, password) => { 
+    cy.visit('/')
+    
+    // Auth Flow
+    // Entrar button
+    cy.contains("Entrar").click()
+    // Email input
+    cy.get('#email').type(email)
+    // Password input
+    cy.get('#password').type(password, { log: false })
+    // Login/Entrar button
+    cy.contains("Entrar").click()
+})
 //
 //
 // -- This is a child command --
