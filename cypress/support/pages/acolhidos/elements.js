3512b0d18c27d7ded37b3c79f7860d75b5c9bf89
Julyanne Correia <julyanne.es@gmail.com>
2024-02-28 01:17:59 -0300

    feat: elements from Acolhidos page

---
+++ cypress/support/pages/acolhidos/elements.js
@@ -0,0 +1,25 @@
+class Elements {
+
+    get botaoFiltroFamilia() {
+        return cy.get('.button-filter').contains('Filtros Família')
+    }
+
+    get botaoFiltroAcolhidos() {
+        return cy.get('.button-filter').contains('Filtros Acolhidos')
+    }
+
+    get filtroFamiliaOpcao() {
+        return cy.get("mat-radio-group").children().eq(4).find("mat-radio-button").should("be.visible")
+    }
+
+    get filtroAcolhidosOpcao() {
+        return cy.get("mat-radio-group").children().eq(3).find("mat-radio-button").should("be.visible")
+    }
+
+    get caixaPesquisa() {
+        return cy.get("input[placeholder='Buscar pessoa']")
+    }
+
+}
+
+export default new Elements()
